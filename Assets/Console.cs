d08d6563520d97b0656dd7a5b3b8c3c8087b3ad8
Ed Paradis <legomaniac@gmail.com>
2013-12-14 12:45:38 -0800

    add intro message; make prompt area a little more clear

--- Assets/Console.cs
+++ Assets/Console.cs
@@ -7,26 +7,35 @@ public class Console : MonoBehaviour {
 	public GUIText inputText;
 	public int maxBufferLength;
 	Queue<string> scrollBuffer;
+	bool clearCursor = true;
 
 	// Use this for initialization
 	void Start () {
 		scrollBuffer = new Queue<string>();
-	
+		AddLineToBuffer("'You Only Have One'");
+		AddLineToBuffer("LD24 Entry, (c) 2013 Ed Paradis");
+		inputText.text = "_";
 	}
 	
 	void Update() {
 		// ripped off from Unity's docs for Input.inputString
 		foreach (char c in Input.inputString) 
 		{
+			if(clearCursor)
+			{
+				inputText.text = "";
+				clearCursor = false;
+			}
 			if (c == "\b"[0])
 			{
 				if (inputText.text.Length != 0)
 					inputText.text = inputText.text.Substring(0, inputText.text.Length - 1);
 			} else {
-				if (c == "\n"[0] || c == "\r"[0])
+				if (c == "\n"[0] || c == "\r"[0]) // user has hit <return> or similar
 				{
 					ParseCommand(inputText.text);
-					inputText.text = "";
+					inputText.text = "_";
+					clearCursor = true;
 				}
 				else
 					inputText.text += c;
